921d169bab9b2059dde81bafbd864cacd362aacc
harsha21111 <harshavardan2k17@gmail.com>
2020-12-25 13:02:09 +0530

    react- webapi

---
+++ ReactApp1/ClientApp/src/components/ProductsList.js
@@ -0,0 +1,56 @@
+﻿import React, { Component } from 'react';
+ 
+export class ProductsList extends Component {
+    static displayName = ProductsList.name;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ProductData: [], loading: true
+        };
+    }
+
+    componentDidMount() {console.log("am here 1")
+        //await fetch('https://localhost:44311/product')
+        //    .then(response => response.json())
+        //    .then(ProductData => function () {
+
+        //        this.setState({ ProductData });
+        //    }
+
+        //    );
+        this.test();
+    }
+
+    async test() {
+        const response = await fetch('https://localhost:44311/product');
+        const data = await response.json();
+        this.setState({ ProductData: data, loading: false });
+        console.log(this.state.ProductData);
+    }
+
+    render() {  
+  
+        return (  
+            <section>  
+                <h1>Products List</h1>  
+                <div>
+                    <table>
+                        <thead><tr><th>Product Id</th><th>Product Name</th><th>Product Category</th><th>Product Price</th></tr></thead>
+                        <tbody>
+                            {
+                                this.state.ProductData.map((p, index) => {
+                                    return <tr key={index}><td>{p.productId}</td><td> {p.productName}</td><td>{p.productCategory}</td><td>{p.productPrice}</td></tr>;
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div> 
+  
+  
+            </section>  
+        )  
+    }  
+}  
+
+export default ProductsList;
---
+++ ReactApp1/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ReactAPI1.Models;
+
+namespace ReactAPI1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+       
+        //public class ProductsController : ApiController
+        //{
+            ProductModel[] products = new ProductModel[]
+            {
+                new ProductModel { ProductId = 1, ProductName = "Tomato Soup", ProductCategory = "Groceries", ProductPrice = 1 },
+
+                new ProductModel { ProductId = 2, ProductName = "Yo-yo",     ProductCategory = "Toys", ProductPrice = 3.75M },
+
+                new ProductModel { ProductId = 3, ProductName = "Hammer",     ProductCategory = "Hardware", ProductPrice = 16.99M },
+
+                new ProductModel { ProductId = 4, ProductName = "Sugar", ProductCategory = "Groceries", ProductPrice = 10 },
+
+                new ProductModel { ProductId = 5, ProductName = "Chhota-Bheem", ProductCategory = "Toys", ProductPrice = 15 },
+
+                new ProductModel { ProductId = 6, ProductName = "Printer", ProductCategory = "Hardware", ProductPrice = 120 }
+            };
+
+
+
+        [HttpGet]
+        // GET: api/Products  
+        public List<ProductModel> Get()
+            {
+                return products.ToList();
+            }
+
+            [HttpGet]
+            [Route("{id}")]
+            public IEnumerable<ProductModel> Get1(int id)
+            {
+                var product = products.Where(x => x.ProductId == id).ToList();
+                if (product == null)
+                {
+                    return (IEnumerable<ProductModel>)NotFound();
+                }
+                return product;
+            }
+        
+    }
+}
